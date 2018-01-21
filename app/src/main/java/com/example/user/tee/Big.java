d4769f5013e3502c435dfe337d7d0594686ac6ed
zinoone <biginw2539@gmail.com>
2018-01-21 14:57:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/user/tee/Big.java
@@ -0,0 +1,13 @@
+package com.example.user.tee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Big extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_big);
+    }
+}
